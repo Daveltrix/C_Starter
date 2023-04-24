0044b32c9e01ccac93a554328a67bd41097cde69
Daveltrix <129883357+Daveltrix@users.noreply.github.com>
2023-04-24 21:13:20 +0200

    New proyect in C

---
+++ NewProgram/src/Main.c
@@ -0,0 +1,15 @@
+/*
+ * Main.c
+ *
+ *  Created on: 24 abr 2023
+ *      Author: Usuario
+ */
+
+
+#include <stdio.h>
+
+int main() {
+   printf("Hola mundo en C!\n");
+   printf("Bye mundo en C!\n");
+   return 0;
+}
